863e28085f761b0c3b9e2c177d7bb922dac7c1b2
Felix Kopp <felix-source@phorax.com>
2017-08-05 16:33:24 +0200

    [TASK] Adapt composer constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,38 +1,20 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "mydashboard".
- *
- * Auto generated 18-09-2014 17:24
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'myDashboard',
     'description' => 'Get a cool Dashboard for TYPO3 with basic elements and a extension API to add own widgets! Help me with the Dashboard and develop widgets for your extensions. TYPO3 4.2 is recommended to use the dashboard in combination with the "userdefined Startpage".',
     'category' => 'module',
     'version' => '',
-    'state' => 'beta',
+    'state' => 'stable',
     'uploadfolder' => false,
     'createDirs' => '',
     'clearcacheonload' => false,
     'author' => 'Tim Lochmueller',
     'author_email' => 'webmaster@fruit-lab.de',
     'author_company' => '',
-    'constraints' =>
-    [
-        'depends' =>
-        [
-            'typo3' => '7.6.0-8.7.99',
-        ],
-        'conflicts' =>
-        [
-        ],
-        'suggests' =>
-        [
-        ],
-    ],
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.0-8.7.99'
+        ]
+    ]
 ];
